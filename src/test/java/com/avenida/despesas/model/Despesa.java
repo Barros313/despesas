59fecbe371260699026869f6238d23e96f782902
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-10-16 11:43:55 -0300

    Added model classes
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ src/test/java/com/avenida/despesas/model/Despesa.java
@@ -0,0 +1,10 @@
+package com.avenida.despesas.model;
+
+import java.util.List;
+
+public class Despesa {
+    private String tipo;
+    private String descricao;
+    private List<Parcela> listaParcelas;
+    private int quantidadeParcelas;
+}
